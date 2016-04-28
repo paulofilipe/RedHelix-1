ca7bf19376de7f88a32dffb681d2e0f834409116
Paulo Filipe Dantas <pdantas@lenovo.com>
2016-04-28 10:57:05 -0300

    add gitignore remove not used modules

--- redhx-build-all/redhx-server-util/src/main/java/org/redhelix/server/util/RedHelixClientReport.java
+++ redhx-build-all/redhx-server-util/src/main/java/org/redhelix/server/util/RedHelixClientReport.java
@@ -17,9 +17,12 @@ import java.net.URISyntaxException;
 import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
 import org.redhelix.core.chassis.RedHxChassisCollection;
 import org.redhelix.core.chassis.RedHxChassisParseException;
 import org.redhelix.core.computer.system.RedHxComputerSystemCollection;
+import org.redhelix.core.manager.RedHxManagerCollection;
+import org.redhelix.core.manager.RedHxManagerParseException;
 import org.redhelix.core.output.stream.RedHxPrintCollectionsHelper;
 import org.redhelix.core.service.root.RedHxTcpProtocolTypeEnum;
 import org.redhelix.core.util.RedHxColumnOutputFormatter;
@@ -27,19 +30,22 @@ import org.redhelix.core.util.RedHxHttpResponseException;
 import org.redhelix.core.util.RedHxParseException;
 import org.redhelix.core.util.RedHxRedfishProtocolVersionEnum;
 import org.redhelix.core.util.RedHxUriPath;
+import org.redhelix.server.lib.manager.RedHxManagerCollectionReader;
+import org.redhelix.server.lib.manager.RedHxManagerPathCollectionReader;
 import org.redhelix.server.lib.reader.chassis.RedHxChassisCollectionReader;
 import org.redhelix.server.lib.reader.chassis.RedHxChassisPathCollectionReader;
 import org.redhelix.server.lib.reader.computer.system.RedHxComputerSystemCollectionReader;
 import org.redhelix.server.lib.reader.util.RedHxServerConnectionContext;
 
 /**
- * A simple way to test reading the Redfish Chassis JSON mockups from the DMTF. This read all
- * Redfish chassis objects from a HTTP connection and the Redfish computer systems with the chassis
- * and prints them out on the console. To run this requires a server implementingthe Redfish
- * protocol or the Redfish mockup DMTF DSP2043_0.99.0a. In either case the the TCP port of the
- * Redfish server must defined by system properties(below). If you are using DSP2043_0.99.0a each of
- * the Redfish mockup chassis files named index.html files has to be modified and switch the field
- * names "Name" and "Description".
+ * A simple way to test reading the Redfish Chassis JSON mockups from the DMTF.
+ * This read all Redfish chassis objects from a HTTP connection and the Redfish
+ * computer systems with the chassis and prints them out on the console. To run
+ * this requires a server implementingthe Redfish protocol or the Redfish mockup
+ * DMTF DSP2043_0.99.0a. In either case the the TCP port of the Redfish server
+ * must defined by system properties(below). If you are using DSP2043_0.99.0a
+ * each of the Redfish mockup chassis files named index.html files has to be
+ * modified and switch the field names "Name" and "Description".
  * <p>
  * This requires these system properties be set.
  * <ul>
@@ -54,21 +60,21 @@ import org.redhelix.server.lib.reader.util.RedHxServerConnectionContext;
  */
 public class RedHelixClientReport {
 
-  private static final String PROP_NAME_PROTOCOL = "param_protocol";
-  private static final String PROP_NAME_HOST_NAME = "param_hostname";
-  private static final String PROP_NAME_REMOTE_TCP_PORT = "param_port";
-  private static final String PROP_NAME_HTTP_USER_NAME = "param_username";
-  private static final String PROP_NAME_HTTP_PASSWORD = "param_password";
+	private static final String PROP_NAME_PROTOCOL = "param_protocol";
+	private static final String PROP_NAME_HOST_NAME = "param_hostname";
+	private static final String PROP_NAME_REMOTE_TCP_PORT = "param_port";
+	private static final String PROP_NAME_HTTP_USER_NAME = "param_username";
+	private static final String PROP_NAME_HTTP_PASSWORD = "param_password";
 
-  /**
-   * @param args
-   */
-  public static void main(String[] args) {
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
 
-    /**
-     * turn off logging
-     */
-    System.setProperty("org.slf4j.simpleLogger.defaultLogLevel", "error");
+		/**
+		 * turn off logging
+		 */
+		System.setProperty("org.slf4j.simpleLogger.defaultLogLevel", "error");
 
     /**
      * When building using command <i>maven package</i> the linux command line<br>
@@ -78,136 +84,158 @@ public class RedHelixClientReport {
      */
     final String protocol = System.getProperty(PROP_NAME_PROTOCOL);
 
-    logMissingProperty(protocol, PROP_NAME_PROTOCOL);
+		logMissingProperty(protocol, PROP_NAME_PROTOCOL);
 
     final String hostname = System.getProperty(PROP_NAME_HOST_NAME);
 
-    logMissingProperty(hostname, PROP_NAME_HOST_NAME);
+		logMissingProperty(hostname, PROP_NAME_HOST_NAME);
 
     final String port = System.getProperty(PROP_NAME_REMOTE_TCP_PORT);
 
-    logMissingProperty(port, PROP_NAME_REMOTE_TCP_PORT);
+		logMissingProperty(port, PROP_NAME_REMOTE_TCP_PORT);
 
-    /**
-     * get the optional HTTP basic security params.
-     */
-    final String username = System.getProperty(PROP_NAME_HTTP_USER_NAME);
-    final String password = System.getProperty(PROP_NAME_HTTP_PASSWORD);
+		/**
+		 * get the optional HTTP basic security params.
+		 */
+		final String username = System.getProperty(PROP_NAME_HTTP_USER_NAME);
+		final String password = System.getProperty(PROP_NAME_HTTP_PASSWORD);
 
-    if ((protocol != null) && (hostname != null) && (port != null)) {
-      final int portNum = Integer.parseInt(port);
+		if ((protocol != null) && (hostname != null) && (port != null)) {
+			final int portNum = Integer.parseInt(port);
 
-      openServerConnection(protocol, hostname, portNum, username, password);
-    } else {
-      printUsage();
-    }
-  }
+			openServerConnection(protocol, hostname, portNum, username, password);
+		} else {
+			printUsage();
+		}
+	}
 
-  private static void logMissingProperty(String propertyValue, String propertyName) {
-    if (propertyValue == null) {
-      System.out.println("Error:The system property \"" + propertyName + "\" is not present.");
-    }
-  }
+	private static void logMissingProperty(String propertyValue, String propertyName) {
+		if (propertyValue == null) {
+			System.out.println("Error:The system property \"" + propertyName + "\" is not present.");
+		}
+	}
 
-  /**
-   * open a connection to a Redfish enabled server and read the chassis and Computer System JSON
-   * messages.
-   *
-   * @param protocol
-   * @param hostname
-   * @param portNum
-   */
-  private static void openServerConnection(String protocol, String hostname, int portNum,
-      String username, String password) {
-    final RedHxTcpProtocolTypeEnum httpProtocol =
-        (protocol.equals("https")) ? RedHxTcpProtocolTypeEnum.HTTPS : RedHxTcpProtocolTypeEnum.HTTP;
+	/**
+	 * open a connection to a Redfish enabled server and read the chassis and
+	 * Computer System JSON messages.
+	 *
+	 * @param protocol
+	 * @param hostname
+	 * @param portNum
+	 */
+	private static void openServerConnection(String protocol, String hostname, int portNum, String username,
+			String password) {
+		final RedHxTcpProtocolTypeEnum httpProtocol = (protocol.equals("https")) ? RedHxTcpProtocolTypeEnum.HTTPS
+				: RedHxTcpProtocolTypeEnum.HTTP;
 
-    /*
-     * create a communication context that will be used to talk with a single Redfish server. This
-     * does not allocate and nextwork sockets.
-     */
-    final RedHxServerConnectionContext ctx;
+		/*
+		 * create a communication context that will be used to talk with a
+		 * single Redfish server. This does not allocate and nextwork sockets.
+		 */
+		final RedHxServerConnectionContext ctx;
 
-    if (username == null) {
-      ctx = new RedHxServerConnectionContext(RedHxRedfishProtocolVersionEnum.VERSION_1,
-          httpProtocol, hostname, portNum);
-    } else {
-      ctx = new RedHxServerConnectionContext(RedHxRedfishProtocolVersionEnum.VERSION_1,
-          httpProtocol, hostname, portNum, username, password);
-    }
+		if (username == null) {
+			ctx = new RedHxServerConnectionContext(RedHxRedfishProtocolVersionEnum.VERSION_1, httpProtocol, hostname,
+					portNum);
+		} else {
+			ctx = new RedHxServerConnectionContext(RedHxRedfishProtocolVersionEnum.VERSION_1, httpProtocol, hostname,
+					portNum, username, password);
+		}
 
-    try {
-      /**
-       * open a HTTP connection to the Redfish server provided by the DMTF mockup. The mockup server
-       * is started with the command "node server.js"
-       *
-       */
-      ctx.openConnection();
+		try {
+			/**
+			 * open a HTTP connection to the Redfish server provided by the DMTF
+			 * mockup. The mockup server is started with the command
+			 * "node server.js"
+			 *
+			 */
+			ctx.openConnection();
 
-      Set<RedHxUriPath> chassisPathSet;
+			Set<RedHxUriPath> chassisPathSet;
 
-      try {
-        /*
-         * read all the chasiss information from the Redfish server. The following three steps are
-         * the heart of this program. The form the RedHelix API and all that is necessary to convert
-         * from the Redfish JSON messages describing all the chassis and computer systems on a
-         * Redfish enabled server to Java classes. All RedHelix raaders classes have a single static
-         * method to read data. This allows them be used in a multi-threaded application with out
-         * locking or contention between Java threads. This allows RedHelix to scale to large
-         * numbers of server.
-         *
-         * First Step. Read the paths to each chassis in the Redfish server. There may be multiple
-         * chasisis Redfish is aware of.
-         */
-        chassisPathSet = RedHxChassisPathCollectionReader.readChassisCollection(ctx);
+			try {
+				/*
+				 * read all the chasiss information from the Redfish server. The
+				 * following three steps are the heart of this program. The form
+				 * the RedHelix API and all that is necessary to convert from
+				 * the Redfish JSON messages describing all the chassis and
+				 * computer systems on a Redfish enabled server to Java classes.
+				 * All RedHelix raaders classes have a single static method to
+				 * read data. This allows them be used in a multi-threaded
+				 * application with out locking or contention between Java
+				 * threads. This allows RedHelix to scale to large numbers of
+				 * server.
+				 *
+				 * First Step. Read the paths to each chassis in the Redfish
+				 * server. There may be multiple chasisis Redfish is aware of.
+				 */
+				chassisPathSet = RedHxChassisPathCollectionReader.readChassisCollection(ctx);
 
-        /**
-         * Second Step. For each path describing a chassis read all the JSON messages.
-         */
-        RedHxChassisCollection chassisCollection =
-            RedHxChassisCollectionReader.readPaths(ctx, chassisPathSet);
+				/**
+				 * Second Step. For each path describing a chassis read all the
+				 * JSON messages.
+				 */
+				RedHxChassisCollection chassisCollection = RedHxChassisCollectionReader.readPaths(ctx, chassisPathSet);
 
-        if (!chassisCollection.isEmpty()) {
-          /*
-           * Thrid Step. Read all Redfish computer systems found within the chassis colllection.
-           */
-          RedHxComputerSystemCollection computerSystemCollection =
-              RedHxComputerSystemCollectionReader.readPaths(ctx, chassisCollection);
+				if (!chassisCollection.isEmpty()) {
+					/*
+					 * Thrid Step. Read all Redfish computer systems found
+					 * within the chassis colllection.
+					 */
+					RedHxComputerSystemCollection computerSystemCollection = RedHxComputerSystemCollectionReader
+							.readPaths(ctx, chassisCollection);
 
-          /**
-           * Now all chassis and Computer System data has been read from the Redfish server so print
-           * it out on stdio. todo print on in section order. A bug is in printing in alpha order
-           * and all blades have the same prompt
-           */
-          RedHxPrintCollectionsHelper.printCollections(
-              RedHxColumnOutputFormatter.PrintOrder.SECTION, chassisCollection,
-              computerSystemCollection);
-        }
-      } catch (RedHxChassisParseException ex) {
-        Logger.getLogger(RedHelixClientReport.class.getName()).log(Level.SEVERE, null, ex);
-      } catch (RedHxHttpResponseException ex) {
-        Logger.getLogger(RedHelixClientReport.class.getName()).log(Level.SEVERE, null, ex);
-      } catch (RedHxParseException ex) {
-        Logger.getLogger(RedHelixClientReport.class.getName()).log(Level.SEVERE, null, ex);
-      }
-    } catch (URISyntaxException ex) {
-      Logger.getLogger(RedHelixClientReport.class.getName()).log(Level.SEVERE, null, ex);
-    } catch (RedHxHttpResponseException ex) {
-      Logger.getLogger(RedHelixClientReport.class.getName()).log(Level.SEVERE, null, ex);
-    }
-  }
+					/**
+					 * Now all chassis and Computer System data has been read
+					 * from the Redfish server so print it out on stdio. todo
+					 * print on in section order. A bug is in printing in alpha
+					 * order and all blades have the same prompt
+					 */
+					RedHxPrintCollectionsHelper.printChassisAndComputerSystemCollections(RedHxColumnOutputFormatter.PrintOrder.ALPHA,
+							chassisCollection, computerSystemCollection);
+					
+					/**
+					 * Read and print all managers read from the redfish server
+					 */
+					readAndPringManagers(ctx);
+				}
+			} catch (RedHxChassisParseException ex) {
+				Logger.getLogger(RedHelixClientReport.class.getName()).log(Level.SEVERE, null, ex);
+			} catch (RedHxHttpResponseException ex) {
+				Logger.getLogger(RedHelixClientReport.class.getName()).log(Level.SEVERE, null, ex);
+			} catch (RedHxParseException ex) {
+				Logger.getLogger(RedHelixClientReport.class.getName()).log(Level.SEVERE, null, ex);
+			} catch (RedHxManagerParseException e) {
+				e.printStackTrace();
+			}
+		} catch (URISyntaxException ex) {
+			Logger.getLogger(RedHelixClientReport.class.getName()).log(Level.SEVERE, null, ex);
+		} catch (RedHxHttpResponseException ex) {
+			Logger.getLogger(RedHelixClientReport.class.getName()).log(Level.SEVERE, null, ex);
+		}
+	}
 
-  private static void printUsage() {
-    System.out.println("Usage: ");
-    System.out.println("Required Parameters:  ");
-    System.out.println(PROP_NAME_PROTOCOL + "=https | http");
-    System.out.println(PROP_NAME_HOST_NAME + "=redfishServerName");
-    System.out.println(PROP_NAME_REMOTE_TCP_PORT + "=redfishServerPortNumber");
-    System.out.println("Optional Parameters:  ");
-    System.out.println(PROP_NAME_HTTP_USER_NAME + "=httpUserName");
-    System.out.println(PROP_NAME_HTTP_PASSWORD + "=httpPassword");
-    System.out.println("Example command:");
-    System.out.println(
-        "java -Dparam_protocol=\"http\" -Dparam_hostname=\"localhost\" -Dparam_port=\"9080\" -jar ./redhx-server-util/target/redhx-server-util-0.1-SNAPSHOT.jar");
-  }
+	private static void readAndPringManagers(RedHxServerConnectionContext ctx) throws RedHxManagerParseException, RedHxHttpResponseException, URISyntaxException, RedHxParseException {
+		
+		Set<RedHxUriPath> redHxManagerPathCollection =  RedHxManagerPathCollectionReader.readManagersCollection(ctx);
+		
+		RedHxManagerCollection managerCollection = RedHxManagerCollectionReader.readPaths(ctx, redHxManagerPathCollection);
+		
+		RedHxPrintCollectionsHelper.pringManagersCollections(RedHxColumnOutputFormatter.PrintOrder.ALPHA, managerCollection);
+		
+	}
+
+	private static void printUsage() {
+		System.out.println("Usage: ");
+		System.out.println("Required Parameters:  ");
+		System.out.println(PROP_NAME_PROTOCOL + "=https | http");
+		System.out.println(PROP_NAME_HOST_NAME + "=redfishServerName");
+		System.out.println(PROP_NAME_REMOTE_TCP_PORT + "=redfishServerPortNumber");
+		System.out.println("Optional Parameters:  ");
+		System.out.println(PROP_NAME_HTTP_USER_NAME + "=httpUserName");
+		System.out.println(PROP_NAME_HTTP_PASSWORD + "=httpPassword");
+		System.out.println("Example command:");
+		System.out.println(
+				"java -Dparam_protocol=\"http\" -Dparam_hostname=\"localhost\" -Dparam_port=\"9080\" -jar ./redhx-server-util/target/redhx-server-util-0.1-SNAPSHOT.jar");
+	}
 }
